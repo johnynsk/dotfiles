eac1c465eeec8675f7fa289d0feb5966edd4ebb9
Evgeniy Vasilev <github+dev@johnynsk.ru>
2016-02-18 21:13:20 +0600

    changes for apt - debian (jessy)

--- init/apt.sh
+++ init/apt.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get install byzanz guake mocp php5-cli nginx-extras vim-nox notify-send memcached thunar
+sudo apt-get install byzanz guake moc php5-cli nginx-extras vim-nox memcached thunar
