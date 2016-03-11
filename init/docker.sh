f3bcf0eb6d6c70bd2fb5ae0e004c347958d6e6f3
Evgeniy Vasilev <e.vasilev@office.ngs.ru>
2016-03-11 10:25:34 +0600

    docker: docker-compose installation

--- init/docker.sh
+++ init/docker.sh
@@ -11,3 +11,7 @@ sudo apt-cache policy docker-engine
 sudo apt-get -y install docker-engine
 sudo service docker start
 sudo usermod -a -G docker ${USER}
+
+sudo bash -c "curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
+
