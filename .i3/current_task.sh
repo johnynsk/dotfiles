ba9ef3a4701fd39a4123c662fb1d5696259a593a
Evgeniy Vasilev <e.vasilev@office.ngs.ru>
2016-03-30 10:32:54 +0600

    jira: fix opening linking task

--- .i3/current_task.sh
+++ .i3/current_task.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-TASK=`source ~/.config/jira && curl -u $user:$password http://jira.rn/rest/api/2/search?jql=labels=jwh:$user:in-work 2>/dev/null | sed -rn 's/.*"key":"([A-Z]+)-([0-9]+)".*/http\:\/\/jira.rn\/browse\/\1-\2/p'`
+TASK=`source ~/.config/jira && curl -u $user:$password http://jira.rn/rest/api/2/search?jql=labels=jwh:$user:in-work 2>/dev/null | sed -rn 's/.*"key":"([A-Z]+)-([0-9]+)".*project.*/http\:\/\/jira.rn\/browse\/\1-\2/p'`
 google-chrome ${TASK}
