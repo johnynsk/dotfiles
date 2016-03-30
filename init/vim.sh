52297d2eafd9b1b926fdd3da4b8ab562633bf4d7
Evgeniy Vasilev <e.vasilev@office.ngs.ru>
2016-03-30 11:47:03 +0600

    vim: first-time exports

--- init/vim.sh
+++ init/vim.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-echo "export EDITOR=vim" >> ~/.bashrc
-echo "export TERM=xterm-256color" >> ~/.bashrc
+
+if [[ -z "`cat ~/.bashrc | grep EDITOR`" ]]
+then
+    echo "export EDITOR=vim" >> ~/.bashrc
+    echo "export TERM=xterm-256color" >> ~/.bashrc
+fi
+
 mkdir -p ~/.vim/autoload/
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 vim +PluginInstall +qall
